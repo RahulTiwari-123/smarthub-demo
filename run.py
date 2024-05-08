433c32ad7f1c5df2102dc3b60793a00fb5ad4a06
RahulTiwari-123 <rahul.tiwari123@gmail.com>
2024-05-08 13:26:33 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+import streamlit as st
+
+st.title("Smarthub - Demo on OpenAI 4 Turbo")
+
+client = OpenAI(api_key="sk-proj-KbPoSZRHhzhpzsFuPCZ5T3BlbkFJSgx3EBKdN3AyzfOYCtMJ")
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
